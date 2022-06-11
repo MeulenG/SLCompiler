b689ef239ec712b1fe3c5d0968661605196d44b2
Mollenthe4th <mollern2000@outlook.dk>
2022-06-11 22:24:48 +0200

    first commit

---
+++ hello.sl
@@ -0,0 +1,14 @@
+begin program.sl
+
+using 
+
+enter function()
+    print "Hello World"
+end function
+
+enter main()
+    call function()
+end main
+
+
+end program.sl
---
+++ parser.cs
@@ -0,0 +1,28 @@
+using System;
+using System.IO;
+
+class ReadFile {
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0) {
+            Console.WriteLine("File not given");
+        }
+        else {
+            string text = System.IO.File.ReadAllText(args, FileMode.Open, FileMode.Read);
+        }
+    }
+}
+class Parse {
+    public static int Parse() {
+        List<T>() = new List <T>();
+        int acc = 0;
+
+        for (0; text.Length;)
+        {
+            
+
+        }
+
+    }   
+}
+
