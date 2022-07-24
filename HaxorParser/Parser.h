ff04e3c4bfd27037f36ed3908300d55e9e9886ca
Mollenthe4th <mollern2000@outlook.dk>
2022-07-24 23:10:57 +0200

    Ast, Parser and CMake for given subdirectory

---
+++ HaxorParser/Parser.h
@@ -0,0 +1,18 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "../cpplibs/Essentials.h"
+#include "../HaxorLexer/Lexer.h"
+/// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
+/// token the parser is looking at.  getNextToken reads another token from the
+/// lexer and updates CurTok with its results.
+static int CurTok;
+static int getNextToken() { return CurTok = gettok(); }
+
+/// BinopPrecedence - This holds the precedence for each binary operator that is
+/// defined.
+static std::map<char, int> BinopPrecedence;
+
+void MainLoop();
+
+#endif
